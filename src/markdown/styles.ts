b1a13eaf89aa549ea27f994e9c77b115a1207bb6
JPBM135 <jpedrobm0@gmail.com>
2024-07-16 04:31:27 -0700

    chore(styles): change to dark theme

--- src/markdown/styles.ts
+++ src/markdown/styles.ts
@@ -3,33 +3,45 @@ export const HEAD_STYLES = /* html */ `
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css" rel="stylesheet">
 
   <style>
     * {
       font-family: 'Roboto', sans-serif;
     }
 
+    body {
+      background-color: #020617;
+      color: #f1f5f9;
+    }
+
+    a {
+      color: #0ea5e9;
+    }
+
     p {
-      margin: 0;
+      margin: 6px 0px;
       padding: 0;
+      font-size: 14px;
     }
 
     blockquote {
-      border-left: 4px solid #e0e0e0;
+      border-left: 4px solid #334155;
       margin: 0;
-      padding: 0.5rem 1rem;
+      padding: 0.1rem 1rem;
+      border-radius: 4px;
+      background-color: #3341552e;
     }
 
     code {
-      background-color: #f5f5f5;
+      background-color: #1e293b;
       padding: 0.25rem 0.5rem;
       border-radius: 4px;
       font-family: 'Roboto Mono', monospace;
     }
 
     pre {
-      background-color: #f5f5f5;
+      background-color: #1e293b;
       padding: 1rem;
       border-radius: 8px;
       font-family: 'Roboto Mono', monospace;
